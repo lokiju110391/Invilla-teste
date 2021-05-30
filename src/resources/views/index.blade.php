cb38247fd46e8a7cc30ec0c87db2feb7104a1473
Victor Gomes <victor.s.gomes03@gmail.com>
2021-05-30 12:46:07 -0300

    add form

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,7 +4,19 @@
 
 <section>
 	
-	Blau
+	<form  id="form-input" name="form-input" method="POST" action="{{ url('/save-file') }}" enctype="multipart/form-data">  	
+
+		<h2>Subimit your file here</h2>
+
+		<br>		
+	  	<div class="form-group">
+	    	<label>File to process</label>
+	    	<input type="file" class="form-control" id="file_to_process" name="file_to_process" placeholder="Choose a File" accept="text/xml" required>
+	  	</div>
+	  	<br>
+	  	<button type="submit" class="btn btn-primary">Submit</button>
+
+	</form>
 
 </section>
 
